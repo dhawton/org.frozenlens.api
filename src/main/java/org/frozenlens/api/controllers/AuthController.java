973853ba7f43fa3f04bc3d9449fdd3650aa0b957
Daniel Hawton <daniel@hawton.org>
2020-04-17 01:55:15 -0500

    Finish register

--- src/main/java/org/frozenlens/api/controllers/AuthController.java
+++ src/main/java/org/frozenlens/api/controllers/AuthController.java
@@ -1,16 +1,26 @@
 package org.frozenlens.api.controllers;
 
 import org.frozenlens.api.dto.RegisterRequest;
+import org.frozenlens.api.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
-    @PostMapping("/register")
-    public void register(@RequestBody RegisterRequest registerRequest) {
+    private final AuthService authService;
 
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        authService.register(registerRequest);
+        return new ResponseEntity<>("OK", HttpStatus.CREATED);
     }
 }
